41c30a8c33fe17815f9fe06ce9b768ab9e67e70d
Mike Rambil <mikerambil@gmail.com>
2023-04-20 06:46:39 -0500

    Started Routing /api/v1/tours

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side...!');
-});
+// app.get('/', (req, res) => {
+//   res.status(200).send('Hello from the server side...!');
+// });
 
-app.post('/', (req, res) => {
-  res.status(200).send('You can POST to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can POST to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours/', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: { tours },
+  });
 });
 
 const port = 3000;
