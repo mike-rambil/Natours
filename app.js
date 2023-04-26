7d4ae5b128c933286466c5f67fc1f655dfdc88d9
Mike Rambil <mikerambil@gmail.com>
2023-04-26 13:20:47 -0500

    User Routes

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+// 1) MIDDLEWARES
+
+app.use(morgan('dev'));
 app.use(express.json());
 
+// 2) READ FILES
 // app.get('/', (req, res) => {
 //   res.status(200).send('Hello from the server side...!');
 // });
@@ -16,6 +21,8 @@ app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 3) ROUTE HANDLER
 
 const getAllTours = (req, res) => {
   res.status(200).json({
@@ -81,10 +88,38 @@ const deleteTour = (req, res) => {
   });
 };
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App runnning on port ${port} ....`);
-});
+const getAllUsers = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'This route is not yet defined',
+  });
+};
+const getUser = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'This route is not yet defined',
+  });
+};
+const createUsers = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'This route is not yet defined',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'This route is not yet defined',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'This route is not yet defined',
+  });
+};
+
+// 4) ROUTES
 
 // app.get('/api/v1/tours/', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
@@ -92,5 +127,12 @@ app.listen(port, () => {
 // app.delete('/api/v1/tours/:id', deleteTour);
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
-
 app.route('/api/v1/tours/:id').get(getTour).delete(deleteTour);
+app.route('api//v1/users').get(getAllUsers).post(createUsers);
+app.route('api//v1/user/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+// 5) START SERVER
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App runnning on port ${port} ....`);
+});
