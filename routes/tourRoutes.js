a8905bf4479e5e3831a969f8a1034da27d6f0cba
Mike Rambil <mikerambil@gmail.com>
2023-04-26 16:55:45 -0500

    minor changes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,13 +4,6 @@ const router = express.Router();
 
 const tourController = require('../controllers/tourController');
 
-// 4) ROUTES
-
-// app.get('/api/v1/tours/', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
 router
   .route('/')
   .get(tourController.getAllTours)
