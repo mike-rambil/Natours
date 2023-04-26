42b46613285f3d7134d458cb15478768cbd89086
Mike Rambil <mikerambil@gmail.com>
2023-04-26 05:17:03 -0500

    reading url params is set

--- app.js
+++ app.js
@@ -25,6 +25,23 @@ app.get('/api/v1/tours/', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    tour,
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
 
