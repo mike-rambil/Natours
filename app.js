ddceb76195148037ec5114209c98a2f0bbcb7aca
Mike Rambil <mikerambil@gmail.com>
2023-04-28 08:05:24 -0500

    proccessed config.env file

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-app.use(morgan('dev'));
+// 1) MIDDLEWARES
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App runnning on port ${port} ....`);
