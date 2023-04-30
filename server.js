1eb70c07f586fa9174803b753c7af95c10936095
Mike Rambil <mikerambil@gmail.com>
2023-04-30 12:27:07 -0500

    Trying Mongoose

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
+const mongoose = require('mongoose');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+const DB =
+  'mongodb+srv://mike:eILx7luQJQnyTaAc@cluster0.dsrld9r.mongodb.net/natours?retryWrites=true&w=majority';
+console.log(DB);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB Connection Successful...');
+  })
+  .catch((err) => console.log('Error connecting to MongoDB Atlas:', err));
 
 const app = require('./app');
 
