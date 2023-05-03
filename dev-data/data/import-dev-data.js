21a9a12ae98ebd2dfc68f163ddb91e899d7b5b32
Mike Rambil <mikerambil@gmail.com>
2023-05-03 16:27:09 -0500

    Added database

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+console.log(DB);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB Connection Successful...');
+  })
+  .catch((err) => console.log('Error connecting to MongoDB Atlas:', err));
+
+//READ FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Deleted');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
