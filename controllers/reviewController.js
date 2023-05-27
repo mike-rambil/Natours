0520094fef5ac3167d04cc06ac47852c5577e4c1
mike <mikerambil@gmail.com>
2023-05-27 17:30:20 -0500

    minor changes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -14,7 +14,7 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 exports.createReview = catchAsync(async (req, res, next) => {
   //Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id;
+  if (!req.body.user) req.body.user = req.user._id;
 
   const newReview = await Review.create(req.body);
   res.status(201).json({
