247a6942a74677528e65ad973d9ccdb4fd168a1e
mike <mikerambil@gmail.com>
2023-05-28 16:02:05 -0500

    Added Pug Templates

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
+
+//Pug setup
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+// app.use(express.static(`${__dirname}/public`));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -19,7 +27,6 @@ const reviewRouter = require('./routes/reviewRoutes');
 app.use(morgan('dev'));
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -27,6 +34,10 @@ app.use((req, res, next) => {
   next();
 });
 
+//---ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
