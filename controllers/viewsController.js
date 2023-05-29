2401bcb97f4813b2c3d98ff9b0f2122fb9dda9fe
mike <mikerambil@gmail.com>
2023-05-29 16:25:40 -0500

    Added cards with loop  iterations

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // Get tour data from the database
+  const tours = await Tour.find();
+  // Build template
+
+  // Render the template
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
