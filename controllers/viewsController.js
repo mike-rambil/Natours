d83c3a71d733244db976ff438b6210c2dee2a0f7
mike <mikerambil@gmail.com>
2023-05-29 18:23:28 -0500

    Added Tour Pages

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,19 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // Get tour data from the database along with guides and reviews
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  // Build template
+
+  // Render the template
+
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
